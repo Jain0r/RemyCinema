5dc042993c3c6785bc42a476f7cbaf5dac0661fa
Jainor <mjrs.1262@gmail.com>
2023-03-01 20:34:32 -0500

    corrigiendo cors config, agregando config en clase principal

--- RemyCinemaBack/src/main/java/com/delfinoapps/remicinemaapi/RemicinemaapiApplication.java
+++ RemyCinemaBack/src/main/java/com/delfinoapps/remicinemaapi/RemicinemaapiApplication.java
@@ -2,6 +2,9 @@ package com.delfinoapps.remicinemaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RemicinemaapiApplication {
@@ -10,4 +13,18 @@ public class RemicinemaapiApplication {
 		SpringApplication.run(RemicinemaapiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+
+				registry.addMapping("*/**").allowedOrigins("http://localhost:5050")
+						.allowedMethods("*")
+						.allowedHeaders("*");
+
+				//WebMvcConfigurer.super.addCorsMappings(registry);
+			}
+		};
+	}
 }
